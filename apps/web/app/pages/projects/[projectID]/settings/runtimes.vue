156498bdaa09137b63040c3ea6c5f09a89d13f29
Sander <brantje@gmail.com>
2026-09-12 12:03:15 +0200

    refactor(#81): remove project runtime settings page

--- apps/web/app/pages/projects/[projectID]/settings/runtimes.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">const route = useRoute()</script>
-<template>
-  <SettingsShell>
-    <ConfigManager :key="String(route.params.projectID)" kind="runtimes" :project-id="String(route.params.projectID)" />
-  </SettingsShell>
-</template>
