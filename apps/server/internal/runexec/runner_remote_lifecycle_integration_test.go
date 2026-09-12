460e59f667faf3ac8ec7b13a230a07d12e2311d3
Sander <brantje@gmail.com>
2026-09-12 12:54:32 +0200

    test(#81): wire remote runner lifecycle without runtimes

--- apps/server/internal/runexec/runner_remote_lifecycle_integration_test.go
+++ apps/server/internal/runexec/runner_remote_lifecycle_integration_test.go
@@ -53,7 +53,7 @@ func TestRemoteGitExternalRunnerLifecycleContinuesAcrossRunners(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	services, err := app.NewServicesWithRuntimes(database, materializer, nil)
+	services, err := app.NewExecutionServices(database, materializer)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -106,7 +106,7 @@ func TestRemoteGitExternalRunnerLifecycleContinuesAcrossRunners(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	processor, err := NewProcessor(services.ExecutionStore, services.ExecutionContext, services.RuntimeInstances, services.ExecutionSessions, engines, recorder, output, git, NewRegistryConnector(services.ControlPlane.Runners.Connections))
+	processor, err := NewProcessor(services.ExecutionStore, services.ExecutionContext, services.ExecutionSessions, engines, recorder, output, git, NewRegistryConnector(services.ControlPlane.Runners.Connections))
 	if err != nil {
 		t.Fatal(err)
 	}
