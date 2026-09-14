de42252da004e3b10d540d502b5307d17aeae261
Sander <brantje@gmail.com>
2026-09-14 10:23:12 +0200

    feat(#85): add project runner settings route

---
+++ apps/web/app/pages/projects/[projectID]/settings/runners.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+const projectId = computed(() => String(route.params.projectID))
+</script>
+
+<template>
+  <SettingsShell>
+    <ProjectRunnerSettings :key="projectId" :project-id="projectId" />
+  </SettingsShell>
+</template>
